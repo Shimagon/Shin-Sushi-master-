1d8be8b95e7f5ecb2c894b6549b5fad1f75b4be3
jy-1105 <cvz12345@gmail.com>
2025-12-01 23:12:30 +0900

    sitting

---
+++ Assets/Spawn/SeatPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatPoint : MonoBehaviour
+{
+    [Header("椅子番号（1〜6など）")]
+    public int seatId;
+
+    [Header("この席が使用中かどうか")]
+    public bool isOccupied = false;
+}
